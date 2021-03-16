759a4e8a71f2a4b9ad0eecc86aea9ccbb869b9bf
Zirafnik <dh@ptica.com>
2021-03-16 18:40:14 +0100

    practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const initialCount= 0;
+  const [count, setCount] = useState(initialCount);
+  const [color, setColor] = useState('black');
+
+  function incrementCount() {
+    setCount(count + 1);
+  };
+
+  function decrementCount() {
+    setCount(count - 1);
+  }
+
+  function resetCount() {
+    setCount(initialCount);
+  }
+
+  useEffect(() => {
+    function changeColorOnClick() {
+      if(color==='black') {
+        setColor('red');
+      } else {
+        setColor('black');
+      }
+    };
+
+    document.getElementById('myDiv').addEventListener('click', changeColorOnClick);
+
+    return () => {
+      document.getElementById('myDiv').removeEventListener('click', changeColorOnClick);
+    }
+
+  }, [color]);
+
+  useEffect(() => {
+    document.title= `Clicked ${count} times`;
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+      <button onClick={resetCount}>Reset</button>
+
+      <div
+        id="myDiv"
+        style={{
+          color: "white",
+          width: "100px",
+          height: "100px",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          backgroundColor: color,
+      }}>This div changes color. Click me!</div>
     </div>
+
   );
 }
 
